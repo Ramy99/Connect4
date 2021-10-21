695f8ff3df75b3a7a3a10938d7f4942b48488c96
Will <wshayne@uvm.edu>
2021-10-21 15:06:48 -0400

    intial testing

---
+++ bitboard_testing.py
@@ -0,0 +1,6 @@
+class Board(object):
+    def __init__(self, player1board, player2board):
+        self.player1_board = player1board
+        self.player2_board = player2board
+    def add_piece(self, player, col):
+        
