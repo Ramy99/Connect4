e581731f115790a1ce5f1805863522341d4b066f
Will Shayne <wshaynespam@gmail.com>
2021-10-21 15:09:13 -0400

    More work on bitboard

--- bitboard_testing.py
+++ bitboard_testing.py
@@ -1,5 +1,5 @@
 class bitboard:
-    def __init__(self, board_one = 0, board_two = 0, num_cols = 7, board_height = 6, column_counts = None):
+    def __init__(self, board_one = 0, board_two = 0, num_cols = 7, board_height = 7, column_counts = None):
         self.player_1 = board_one
         self.player_2 = board_two
         self.num_cols = num_cols
@@ -17,9 +17,27 @@ class bitboard:
         else:
             self.player_2 = self.player_2 | add
         self.column_counts[col] += 1
+    def check_win(self, player):
+        if player == 1:
+            testboard = self.player_1
+        else:
+            testboard = self.player_2
+        #test vertical wins, just shift by 3
+        if testboard & (testboard << 1) & (testboard << 2) & (testboard << 3):
+            return True
+        #test horizontal wins, shift by board height 3 times
+        elif testboard & (testboard << self.board_height) & (testboard << self.board_height * 2) & (testboard << self.board_height * 3):
+            return True
+        #test diagonal wins, shift by board height + 1 3 times, then board height - 1 3 times
+        elif testboard & (testboard << self.board_height + 1) & (testboard << (self.board_height + 1) * 2) & (testboard << (self.board_height + 1) * 3):
+            return True
+        elif testboard & (testboard << self.board_height - 1) & (testboard << (self.board_height - 1) * 2) & (testboard << (self.board_height - 1) * 3):
+            return True
+        else:
+            return False
     def print_board(self):
         boardlist = []
-        for row in range(self.board_height):
+        for row in range(self.board_height - 1):
             row_list = []
             for c in range(self.num_cols):
                 square = 1 << (c * self.board_height + row)
@@ -33,3 +51,40 @@ class bitboard:
         for i in range(len(boardlist)):
             boardlist[i] = ' | '.join(boardlist[i])
         return '| ' + ' |\n\n| '.join(boardlist) + ' |'
+    def player_list(self, player):
+        if player == 1:
+            board = self.player_1
+        else:
+            board = self.player_2
+        l = []
+        for i in range(self.num_cols * (self.board_height - 1)):
+            if board & 1<<i:
+                l.insert(i, 1)
+            else:
+                l.insert(i, 0)
+        return l
+    def list_legal_moves(self):
+        legal = [i for i in range(self.num_cols) if self.column_counts[i] < self.board_height - 1]
+        return legal
+
+def get_input(board):
+    col = int(input("What column? "))
+    while not col in board.list_legal_moves():
+        col = int(input("Choose a legal column: "))
+    return col
+
+def main():
+    b = bitboard()
+    to_play = 1
+    while True:
+        print(b.print_board())
+        col = get_input(b)
+        b.add_piece(to_play, col)
+        if b.check_win(to_play):
+            print(b.print_board())
+            print(f"Player {to_play} wins!")
+            break
+        if to_play == 1:
+            to_play = 2
+        else:
+            to_play = 1
